ab996ee83a9821ea2a73057d46d569eb6527dbcb
discopatrick <git@discopatrick.me>
2016-04-08 13:11:11 +0100

    uses the basic SearchForm as the form_class for the view

--- djangasticsearch/search/views.py
+++ djangasticsearch/search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from haystack.generic_views import SearchView
+from haystack.forms import SearchForm
 
 class ContactSearch(SearchView):
 	template_name = 'search/search-generic-view.html'
+	form_class = SearchForm
