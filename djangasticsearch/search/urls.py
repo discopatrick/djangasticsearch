1d02e93ed5007c9bc55b8d678164615da1f40222
discopatrick <git@discopatrick.me>
2016-05-09 19:35:15 +0100

    adds a word search view/url

--- djangasticsearch/search/urls.py
+++ djangasticsearch/search/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ContactSearch.as_view(), name='search_contact_search'),
+    url(r'^word/$', views.WordSearch.as_view(), name='search_word_search'),
 ]
